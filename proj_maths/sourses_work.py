b1d89d8bf5c272e7fdd08a2b1dafa3f40ecb99fc
SamodelkinDA <samodelkin.da@phystech.edu>
2026-04-03 08:51:10 +0300

    Подключена возможность создания и запоминания новых пользователей

--- proj_maths/sourses_work.py
+++ proj_maths/sourses_work.py
@@ -1,4 +1,20 @@
 
 
-def get_source(sourse_info : dict):
-    return 1
+def get_source(source_info : dict):
+    target_source = -1
+    max_source_id = 0
+    with open("./data/sourses.csv", "r", encoding="utf-8") as sources_file:
+        for line in sources_file.readlines()[1:]:
+            source_id, sourse_name, sourse_email, rank = line.split(";")
+            if sourse_name == source_info['name'] and sourse_email == source_info['email']:
+                target_source = source_id
+                break
+            if int(source_id) > max_source_id:
+                max_source_id = int(source_id)
+    if target_source == -1:
+        with open("./data/sourses.csv", "a", encoding="utf-8") as sources_file:
+            new_source = f"\n{max_source_id + 1};{source_info['name']};{source_info['email']};{0}"
+            sources_file.write(new_source)
+        target_source = max_source_id + 1
+
+    return target_source
